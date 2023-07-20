eabd8e4c5585994e88fa099e1d1065197a3ecf45
arun daniel <itarundaniel@gmail.com>
2023-07-20 05:27:01 -0400

    updated variables

--- variables.tf
+++ variables.tf
@@ -2,5 +2,11 @@ variable region {
     type = string
     default = "us-east-1"
 }
-variable project {}
-variable vpccidr {}
+variable project {
+    type = string
+    default = "ASA"
+}
+variable vpccidr {
+    type = string
+    default = "10.128.131.0/24"
+}
